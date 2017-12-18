a4a7093c24e52e6095d11874314ea1023e980490
sn0re <david@code0.xyz>
2017-12-18 13:51:42 +0100

    added simple timeIt

--- dbg.nim
+++ dbg.nim
@@ -1,3 +1,10 @@
 template dbg*(args: varargs[untyped]) =
   ##like `debugEcho` but removed when compiled with -d:release 
   when not defined release: debugEcho args
+
+template timeIt*(name: string, p: untyped): stmt =
+  ## Performs timing of the block call, and makes output into stdout.
+  let timeStart = cpuTime()
+  for idx in 0..10_000:
+    p
+  echo name, ": ", (cpuTime() - timeStart) * 1000000
