97b5362f439a1bb5abaff03765e0e50f30594bbf
David Krause <enthus1ast@users.noreply.github.com>
2018-07-07 19:16:25 +0200

    fix Error: undeclared identifier: 'stmt'

--- dbg.nim
+++ dbg.nim
@@ -4,7 +4,7 @@ template dbg*(args: varargs[untyped]) =
   ##like `debugEcho` but removed when compiled with -d:release 
   when not defined release: debugEcho args
 
-template timeIt*(name: string, p: untyped): stmt =
+template timeIt*(name: string, p: untyped): untyped =
   ## Performs timing of the block call, and makes output into stdout.
   let timeStart = cpuTime()
   for idx in 0..10_000:
