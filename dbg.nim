0872e42e760779ec7b2ae86c9de88adcf7209fac
David Krause <david@code0.xyz>
2020-02-22 21:15:30 +0100

    added decho
    Signed-off-by: David Krause <david@code0.xyz>

--- dbg.nim
+++ dbg.nim
@@ -1,16 +1,25 @@
 import times
+import macros
+
 
 template dbg*(args: varargs[untyped]) =
-  ##like `debugEcho` but removed when compiled with -d:release 
+  ##like `debugEcho` but removed when compiled with -d:release
   when not defined release: debugEcho args
 
-template timeIt*(name: string, p: untyped): stmt =
+template timeIt*(name: string, p: untyped): untyped =
   ## Performs timing of the block call, and makes output into stdout.
   let timeStart = cpuTime()
   for idx in 0..10_000:
     p
   echo name, ": ", (cpuTime() - timeStart) * 1000000
 
+macro decho*(elem: untyped) =
+  let li = lineInfo(elem)
+  var varname = repr elem
+  quote do:
+    echo ">>> ", `li`, ": ", `varname`, " => ", `elem`
+
 when isMainModule:
   timeIt("foo"):
-    echo "foo"
+    decho "foo"
+
